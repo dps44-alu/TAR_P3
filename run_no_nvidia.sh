37a98c0d6a9a0da42549976092315360202b53e2
Tamai Ramírez Gordillo <106489424+TamaiRamirez@users.noreply.github.com>
2025-01-29 11:03:11 +0100

    Update run_no_nvidia.sh

--- run_no_nvidia.sh
+++ run_no_nvidia.sh
@@ -9,10 +9,3 @@ docker run --rm -it -e DISPLAY=${DISPLAY} \
         --volume="$PWD:/workspace:rw" \
         --name $containerName -e "TERM=xterm-256color"\
         ros_noetic:latest bash
-
-docker run --rm -it --gpus all -e DISPLAY=${DISPLAY} \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw --network host \
-    --workdir="/workspace" \
-    --name $containerName --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=all -e "TERM=xterm-256color" \
-    --volume="$PWD:/workspace:rw" \
-    ros_noetic:latest bash
